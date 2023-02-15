4e6c1c608b14ca247dd3cc9ce5aaf87415e96cb8
ApurvZ <43213555+ApurvZ@users.noreply.github.com>
2023-02-15 18:39:24 +0530

    Add files via upload

---
+++ haydn/AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/evolution_haydn.mk
+
+COMMON_LUNCH_CHOICES := \
+    evolution_haydn-user \
+    evolution_haydn-userdebug \
+    evolution_haydn-eng
